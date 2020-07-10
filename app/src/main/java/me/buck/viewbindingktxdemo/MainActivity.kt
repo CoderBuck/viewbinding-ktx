e56e155fa22d177f02a48dbeaf32ff8e0b56a95c
Buck <coderbuck@163.com>
2020-07-10 17:10:16 +0800

    demo

--- app/src/main/java/me/buck/viewbindingktxdemo/MainActivity.kt
+++ app/src/main/java/me/buck/viewbindingktxdemo/MainActivity.kt
@@ -1,11 +1,17 @@
 package me.buck.viewbindingktxdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.buck.viewbindingktx.viewBinding
+import me.buck.viewbindingktxdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by viewBinding(ActivityMainBinding::bind)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding.tv.text = "by viewbinding-ktx"
     }
 }
